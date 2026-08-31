6b626cc6e9724024e818eaa45345295a7018cafc
Yoshi <43599353+Yoshimura2771@users.noreply.github.com>
2026-08-31 19:33:30 +0900

    Build ROS 2 IK service and kinematics tools

--- src/inverse_kinematics_node.cpp
+++ src/inverse_kinematics_node.cpp
@@ -1,41 +1,36 @@
-#include <cstdio>
-#include <rclcpp/rclcpp.hpp>
-#include <cmath>
-
 #include <memory>
 
+#include "catchrobo2026_msgs/srv/inverse_kinematics.hpp"
+#include "rclcpp/rclcpp.hpp"
 #include "ros2_inverse_kinematics/robot_kinematics.h"
 
+namespace {
 
+using InverseKinematics = catchrobo2026_msgs::srv::InverseKinematics;
 
-using namespace std;
-
-void calc_inverse_kinematics(const std::shared_ptr<	> request, std::shared_ptr<	> response)
+void calculate_inverse_kinematics(
+    const InverseKinematics::Request::SharedPtr request,
+    InverseKinematics::Response::SharedPtr response)
 {
-	robot_kinamatics robot_kinematics;
-
-	
-	robot_kinematics.inverse_kinematics(,);
-
-
-
-	RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"");
-	RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"sending back response:");
-
+    robot_kinematics kinematics;
+    auto target_pose = request->target_pose;
+    kinematics.inverse_kinematics(
+        target_pose.data(), response->joint_angles.data());
 }
 
-int main(int argc, char ** argv)
+}  // namespace
+
+int main(int argc, char **argv)
 {
-  (void) argc;
-  (void) argv;
+    rclcpp::init(argc, argv);
 
-  rclcpp::init(argc, arg);
+    const auto node = rclcpp::Node::make_shared("inverse_kinematics_server");
+    const auto service = node->create_service<InverseKinematics>(
+        "inverse_kinematics", calculate_inverse_kinematics);
+    (void)service;
 
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("inverse_kinematics_server");
-
-  rclcpp::Service<	>::SharedPtr service = node->create_service<	>("inverse_kinematics");
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Inverse kinematics ready");
-
-  rclcpp::spin(node);
-
+    RCLCPP_INFO(node->get_logger(), "Inverse kinematics service is ready");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
